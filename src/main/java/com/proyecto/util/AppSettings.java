cffcbcb3220987d8d043e9b9690a505eda4906c2
mauroar1001 <83997911+mauro-arcelles@users.noreply.github.com>
2022-06-26 19:28:43 -0500

    cross origin to heroku

--- src/main/java/com/proyecto/util/AppSettings.java
+++ src/main/java/com/proyecto/util/AppSettings.java
@@ -1,8 +1,6 @@
 package com.proyecto.util;
 
 public class AppSettings {
+	public static final String URL_CROSS_ORIGIN = "https://daw2-pry-final-frontend.herokuapp.com";
 
-	public static final String URL_CROSS_ORIGIN = "http://localhost:4200";
-	
-	
 }
